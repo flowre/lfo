ebc4ad099f4d5f4a3858e59f2e98969492e66c52
liamge <lg1898@nyu.edu>
2016-04-14 13:43:41 -0400

    Begin to figure out haiku generation

--- markovChain.py
+++ markovChain.py
@@ -2,10 +2,12 @@ __author__ = 'liamgeron'
 import nltk
 import random
 from numpy.random import choice
+from nltk.corpus import cmudict
 
 class MarkovChain:
     def __init__(self):
         self.transitionMatrix = {}
+        self.d = cmudict.dict()
 
     def train(self,states):
         self.states = states
@@ -15,6 +17,29 @@ class MarkovChain:
             self.transitionMatrix[s] = {}
             for a in states:
                 self.transitionMatrix[s][a] = cfd[s][a] / fdist[s]
+                
+    def syllables(self,word):
+    	return [len(list(y for y in x if isdigit(y[-1]))) for x in self.d[word.lower()]]
+    	
+    def haiku_search(self, words):
+    	syl_count = 0
+    	word_count = 0
+    	haiku_line_count = 0
+    	lines = []
+    	for word in words:
+        	syl_count += self.syllables(word)
+        	if haiku_line_count == 0:
+            	if syl_count == 5:
+                	lines.append(word)
+                	haiku_line_count += 1
+        	elif haiku_line_count == 1:
+            	if syl_count == 12:
+                	lines.append(word)
+                	haiku_line_count += 1
+        	else:
+            	if syl_count == 17:
+                	lines.append(word)
+                	haiku_line_count += 1
 
     # Get some start probabilities and instigate a sentence with them
     # incorporate random choice using numpy, that'll end the infinite loop
@@ -23,15 +48,15 @@ class MarkovChain:
         # seed, next = self.states[seed], self.states[seed+1]
         # Gets first bigram
         seed = self.states[random.randint(0,len(self.states))]
-        line1 = []
-        line2 = []
-        line3 = []
-        line1.append(seed)
+        syl_cound = 0
+        word_count = 0
+        haiku_line_count = 0
+        lines = []
         while l <= length:
             seed = choice((list(self.transitionMatrix[seed].keys())), 1, list(self.transitionMatrix[seed].values()))
             print(seed)
-            l += 1
         return ' '.join(generated_states)
+        
 
 training_set = nltk.corpus.brown.words(categories='news')[:100]
 training_set = [w.lower() for w in training_set]
